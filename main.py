af6db6bb2abf609143ee880c4661e00d7e255747
joaodiogoPic <96116442+joaodiogoPic@users.noreply.github.com>
2022-01-18 20:53:47 +0000

    LoraWAN

---
+++ main.py
@@ -0,0 +1,89 @@
+from network import LoRa
+from machine import Pin
+import socket
+import time
+import ubinascii
+import struct
+import machine
+
+# Initialise LoRa in LORAWAN mode .
+lora = LoRa(mode=LoRa.LORAWAN)
+# create an ABP authentication params
+dev_addr = struct.unpack(">l", ubinascii.unhexlify('260BF5EC'))[0]
+nwk_swkey = ubinascii.unhexlify('820C96623ADB35676238F91BA66F0490')
+app_swkey = ubinascii.unhexlify('3A0C703182B116A16077CAFC4CAF6A3C')
+
+# join a network using ABP (Activation By Personalization)
+lora.join(activation=LoRa.ABP, auth=(dev_addr, nwk_swkey, app_swkey))
+
+# wait until the module has joined the network
+while not lora.has_joined():
+    time.sleep(2.5)
+print('Not yet joined... ')
+print('Joined ')
+
+# create a LoRa socket
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+# set the LoRaWAN data rate
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 3)
+gpio = Pin('G23', mode = Pin.OUT)
+ledOn = False
+
+# para ler o pin da adc
+adc = machine.ADC()                                 # create an ADC object
+apin = adc.channel(pin='P16', attn=adc.ATTN_11DB)   # create an analog pin on P16
+
+def check_downlink():
+    try:
+        data = s.recv(64)
+    except OSError as e:
+        print("Receiving data ERROR: ")
+        print(e)
+
+    if not data :
+        return
+
+    print(data)
+    return data
+
+while True : 
+    if(lora.has_joined()):
+        try:
+            val = apin.voltage()                                # read an analog value in milivolts
+            tmpVal = int((val * 100) / 3300)
+            print("Temp: " + str(tmpVal))
+            s.send(bytes([tmpVal]))
+            print(lora.stats())
+        except OSError as e:
+            print("Receiving data ERROR: ")
+            print(e)
+
+        print(gpio.value())
+
+        # make the socket non - blocking
+        s.setblocking(False)
+        # get any data received
+
+        timeRX = 0
+        for i in range(10) :
+            data = check_downlink() 
+            if data != None:
+                print("Time reception: " + str(timeRX))
+            if(data == b'\x01'):
+                ledOn = True
+            elif (data == b'\x02'):
+                ledOn = False
+
+            time.sleep(1)
+            timeRX = timeRX + 1
+ 
+        # Change the gpio value
+        if(ledOn):
+            gpio.value(1)
+        else:
+            gpio.value(0)
+
+        
+        
+
+        

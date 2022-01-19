7c98e08a853a61786c9fca2d5e694e876856240e
joaodiogoPic <96116442+joaodiogoPic@users.noreply.github.com>
2022-01-19 21:29:36 +0000

    Excel updae

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ print('Joined ')
 # create a LoRa socket
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 # set the LoRaWAN data rate
-s.setsockopt(socket.SOL_LORA, socket.SO_DR, 3)
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 0)
 gpio = Pin('G23', mode = Pin.OUT)
 ledOn = False
 
